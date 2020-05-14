ed2c1f0906628abf01c5a1d85a5c4ca89f970baf
petergiteube <peterduvauchelle97@gmail.com>
2020-05-14 10:02:38 +0200

    add css and javascript source in template.php - add manageUserView

--- view/manageUserView.php
+++ view/manageUserView.php
@@ -1,40 +1,28 @@
 <?php include_once "adminHeader.php"; ?>
 
-<div id="content-wrapper" class="d-flex flex-column">
+<div id="content-wrapper col-md-12 col-xs-12" class="d-flex flex-column">
     <div class="container-fluid">
+        <div class="">
         <h1 class="p-3 mb-2 text-dark">Tableaux des utilisateurs</h1>
-
+        <button class="btn btn-outline-secondary">Ajouter un utilisateur</button>
+        </div>
         <table class="table table-bordered">
             <thead>
             <tr>
-                <td scope="col">id utilisateur</td>
-                <td>pseudo</td>
-                <td>nom</td>
-                <td>prenom</td>
-                <td>mail</td>
-                <td>telephone</td>
-                <td>ville</td>
-                <td>role</td>
+                <td scope="col">ID</td>
+                <td>Pseudo</td>
+                <td>Nom</td>
+                <td>Prénom</td>
+                <td>Mail</td>
+                <td>Téléphone</td>
+                <td>Ville</td>
+                <td>Rôle</td>
             </tr>
             </thead>
             <tbody>
             <?php
-            $result = array_map(function ($user) {
-                return "<tr>
-                                <td>" . $user->getId() . "</td>
-                                <td>" . $user->getPseudo() . "</td>
-                                <td>" . $user->getLastName() . "</td>
-                                <td>" . $user->getFirstName() . "</td>
-                                <td>" . $user->getEmail() . "</td>
-                                <td>" . $user->getPhoneNumber() . "</td>
-                                <td>" . $user->getCity() . "</td>
-                                <td>" . $user->getRole() . "</td>
-                            <tr>";
-            }, $requestUser);
-
-
-            for ($i = 0; $i < count($requestUser); $i++) {
-                echo $result[$i];
+            for ($i = 0; $i < count($users); $i++) {
+                echo $users[$i];
             }
             ?>
             </tbody>
--- view/template.php
+++ view/template.php
@@ -5,19 +5,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="public/css/style.css">
-    <link rel="stylesheet" href="public/css/bootstrap/bootstrap.css">
-    <link rel="stylesheet" href="public/css/fontawesome/all.css">
-    <link rel="stylesheet" href="public/css/sb-admin/sb-admin.css">
+    <link rel="stylesheet" href="<?= $this->getIndex() . '/public/css/style.css' ?>">
+    <link rel="stylesheet" href="<?= $this->getIndex() . '/public/css/bootstrap/bootstrap.css' ?>">
+    <link rel="stylesheet" href="<?= $this->getIndex() . '/public/css/fontawesome/all.css' ?>">
+    <link rel="stylesheet" href="<?= $this->getIndex() . '/public/css/sb-admin/sb-admin.css' ?>">
     <title><?= $title ?></title>
 </head>
 <body>
     <?= $header ?>
     <?= $content ?>
 </body>
-<script src="public/js/jquery.js"></script>
-<script src="public/js/popper.js"></script>
-<script src="public/js/fontawesome/all.js"></script>
-<script src="public/js/sb-admin/sb-admin.js"></script>
-<script src="public/js/bootstrap.js"></script>
+<script src="<?= $this->getIndex() . '/public/js/jquery.js' ?>"></script>
+<script src="<?= $this->getIndex() . '/public/js/popper.js' ?>"></script>
+<script src="<?= $this->getIndex() . '/public/js/fontawesome/all.js' ?>"></script>
+<script src="<?= $this->getIndex() . '/public/js/sb-admin/sb-admin.js' ?>"></script>
+<script src="<?= $this->getIndex() . '/public/js/bootstrap.js' ?>"></script>
 </html>
