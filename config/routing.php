62503bf8c184d1732403d689b919fc7e4dec2d42
petergiteube <peterduvauchelle97@gmail.com>
2020-05-30 20:09:50 +0200

    Adding new routes for recipe & user

--- config/routing.php
+++ config/routing.php
@@ -39,4 +39,42 @@ return function(RoutesBuilder $routes) {
     $routes->add("marmiton_manage_user", "/admin/users/manage")
         ->controller([ManageUserController::class, "manage"])
         ->methods(['GET']);
+
+    $routes->add("marmiton_add_user","/admin/user/add")
+        ->controller([UserController::class, "add"])
+        ->methods(['GET','POST']);
+
+    $routes->add("marmiton_edit_user", "/admin/user/:id/edit")
+        ->controller([UserController::class, "edit"])
+        ->methods(['GET']);
+
+    $routes->add("marmiton_update_user", "/admin/user/:id/update")
+        ->controller([UserController::class, "update"])
+        ->methods(['POST']);
+
+    $routes->add("marmiton_delete_user", "/admin/user/delete")
+        ->controller([UserController::class, "delete"])
+        ->methods(['POST']);
+
+    $routes->add("marmiton_add_recipe", "/admin/recipe/add")
+        ->controller([RecipeController::class, "add"])
+        ->methods(['GET','POST']);
+
+    $routes->add("marmiton_delete_recipe", "/admin/recipe/delete")
+        ->controller([RecipeController::class, "delete"])
+        ->methods(['GET', 'POST']);
+
+    $routes->add("marmiton_edit_recipe", "/admin/recipe/:id/edit")
+        ->controller([RecipeController::class, "edit"])
+        ->methods(['GET']);
+
+    $routes->add("marmiton_update_recipe", "/admin/recipe/:id/update")
+        ->controller([RecipeController::class, "update"])
+        ->methods(['POST', 'GET']);
+
+    $routes->add("marmiton_get_ingredient","/admin/recipe/get/ingredient")
+        ->controller([IngredientController::class, "list"])
+        ->methods(['GET']);
+
+   
 };
