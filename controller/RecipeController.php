b0864340a41a14b82e7d2c377737ed22bcb394e9
petergiteube <peterduvauchelle97@gmail.com>
2020-06-02 20:19:12 +0200

    refactoring add and update methods to insert or update image source | adding method to delete recipe | adding methods to add or update step/ingredients from recipe | adding method to get a specific recipe

--- controller/RecipeController.php
+++ controller/RecipeController.php
@@ -5,7 +5,8 @@ use Framework\Http\Response;
 use Framework\Http\Request;
 use Framework\Configuration;
 
-class RecipeController extends Controller {
+class RecipeController extends Controller
+{
 
     private $recipeDao;
     private $recipeCategoryDao;
@@ -15,7 +16,8 @@ class RecipeController extends Controller {
     private $stepDao;
     private $location;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->recipeDao = new RecipeDao();
         $this->recipeCategoryDao = new RecipeCategoryDao();
         $this->ingredientDao = new IngredientDao();
@@ -23,10 +25,11 @@ class RecipeController extends Controller {
         $this->recipeIngredientDao = new RecipeIngredientDao();
         $this->stepDao = new StepDao();
 
-        $this->location = Configuration::get("index") ."/admin/recipes/validate";
+        $this->location = Configuration::get("index") . "/admin/recipes/validate";
     }
 
-    public function validate() {
+    public function validate()
+    {
         $this->denyAccessUnlessGranted("ROLE_ADMIN");
 
         $validRecipes = $this->recipeDao->getValidRecipes();
@@ -35,7 +38,8 @@ class RecipeController extends Controller {
         return Response::view("validateRecipe", ["recipes" => $result]);
     }
 
-    public function waiting() {
+    public function waiting()
+    {
         $this->denyAccessUnlessGranted("ROLE_ADMIN");
 
         $waitingRecipes = $this->recipeDao->getWaitingRecipes();
@@ -44,32 +48,30 @@ class RecipeController extends Controller {
         return Response::view("waitingRecipe", ["recipes" => $result]);
     }
 
-    private function formatHTMLRecipe() {
-        return function($recipe) {
+    private function formatHTMLRecipe()
+    {
+        return function ($recipe) {
             return "<tr id='recipeRow" . $recipe->getId() . "'>
-                <td>".$recipe->getId()."</td>
-                <td>".$recipe->getName()."</td>
-                <td>".$recipe->getCost()."</td>
-                <td>".$recipe->getPrepTime()."</td>
-                <td>".$recipe->getCookingTime()."</td>
-                <td>".$recipe->getReleaseDate()."</td>
-                <td>".$recipe->getHeadCount()."</td>
-                <td><a class='btn btn-secondary' href='".Configuration::get('index')."/admin/recipe/".$recipe->getId()."/edit' role='button'>Edit</a> <button data-id='".$recipe->getId()."'id='delete-recipe' class='btn btn-danger' type='button' data-toggle='modal' data-target='#delete-recipe-modal'>Supprimer</button></td>
+                <td>" . $recipe->getId() . "</td>
+                <td>" . $recipe->getName() . "</td>
+                <td>" . $recipe->getCost() . "</td>
+                <td>" . $recipe->getPrepTime() . "</td>
+                <td>" . $recipe->getCookingTime() . "</td>
+                <td>" . $recipe->getReleaseDate() . "</td>
+                <td>" . $recipe->getHeadCount() . "</td>
+                <td><a class='btn btn-secondary' href='" . Configuration::get('index') . "/admin/recipe/" . $recipe->getId() . "/edit' role='button'>Edit</a> <button data-id='" . $recipe->getId() . "'id='delete-recipe' class='btn btn-danger' type='button' data-toggle='modal' data-target='#delete-recipe-modal'>Supprimer</button></td>
             </tr>";
         };
     }
 
-    public function add(Request $request) {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
-
+    public function add(Request $request)
+    {
         $categories = [];
-        
+
         $post = $request->request();
         $userId = $_SESSION['user']->getId();
-        
-        
-        if($post->count() > 0) {
-            // TODO : return redirection vers une page
+
+        if ($post->count() > 0) {
             $name = $post->get('name');
             $cost = $post->get('cost');
             $prepTime = $post->get('prep_time');
@@ -80,38 +82,61 @@ class RecipeController extends Controller {
             $listQuantity = $post->get('quantity');
             $listUnit = $post->get('unit');
             $listStep = $post->get('step');
-        
-            if(strlen($name) > 0
-            && strlen($cost) > 0
-            && strlen($prepTime) > 0
-            && strlen($cookingTime) > 0
-            && strlen($headcount) > 0
-            && strlen($recipeCategory) > 0
-            && is_array($listIngredient)
-            && is_array($listQuantity)
-            && is_array($listUnit)
-            && is_array($listStep)) {
+            $recipeImageName = $_FILES["recipe-image"]["name"];
+            $recipeImageSize = $_FILES["recipe-image"]["size"];
+
+            if (
+                strlen($name) > 0
+                && strlen($cost) > 0
+                && strlen($prepTime) > 0
+                && strlen($cookingTime) > 0
+                && strlen($headcount) > 0
+                && strlen($recipeCategory) > 0
+                && is_array($listIngredient)
+                && is_array($listQuantity)
+                && is_array($listUnit)
+                && is_array($listStep)
+            ) {
 
                 $releaseDate = date("Y-m-d");
+
                 try {
+                    $VALIDATE_RECIPE = 0;
+                    if ($this->hasRole("ROLE_ADMIN")) {
+                        $VALIDATE_RECIPE = 1;
+                    }
+
                     $mapRecipeCategory = $this->recipeCategoryDao->getRecipeCategoryByName($recipeCategory);
                     $IdRecipeCategory = $mapRecipeCategory->getIdCategory();
-                    $this->recipeDao->insertRecipe($name, $cost, $prepTime, $cookingTime, $releaseDate, 1, $headcount, intval($userId), intval($IdRecipeCategory));
-                    $recipeId = $this->recipeDao->getLastRecipeInsertedId();
 
-                    for($i = 0; $i < count($listIngredient); $i++) {
-                        $ingredientId = $this->ingredientDao->getIngredientIdByName($listIngredient[$i]);
-                        $unitId = $this->unitDao->getUnitByLabel($listUnit[$i]);
-                        $this->recipeIngredientDao->insertIngredientByRecipeId(intval($recipeId),intval($ingredientId),intval($listQuantity[$i]),intval($unitId));
-                    }
+                    $uploadedDate = date("d-m-Y_H-i-s");
 
-                    for($i = 0; $i < count($listStep); $i++) {
-                        $numberStep = $i + 1;
-                        $this->stepDao->insertStep($numberStep, $listStep[$i], $recipeId);
+                    $ext = explode('.', $recipeImageName);
+                    $extensionFile = $ext[1];
+
+                    $newFileName = $ext[0] . '_' . $uploadedDate;
+                    $targetPath = $_SERVER['DOCUMENT_ROOT'] . 'marmitonCnam/public/recipe-images/' . $newFileName . '.' . $extensionFile;
+                    
+                    // Si la taille de l'image est au dessus de 4MB.
+                    if ($recipeImageSize < 4194304) {
+                        if (move_uploaded_file($_FILES['recipe-image']['tmp_name'], $targetPath)) {
+                            $this->recipeDao->insertRecipe($name, $cost, $prepTime, $cookingTime, $releaseDate, $VALIDATE_RECIPE, $headcount, $newFileName . "." . $extensionFile, intval($userId), intval($IdRecipeCategory));
+                            $recipeId = $this->recipeDao->getLastRecipeInsertedId();
+
+                            for ($i = 0; $i < count($listIngredient); $i++) {
+                                $ingredientId = $this->ingredientDao->getIngredientIdByName($listIngredient[$i]);
+                                $unitId = $this->unitDao->getUnitByLabel($listUnit[$i]);
+                                $this->recipeIngredientDao->insertIngredientByRecipeId(intval($recipeId), intval($ingredientId), intval($listQuantity[$i]), intval($unitId));
+                            }
+
+                            for ($i = 0; $i < count($listStep); $i++) {
+                                $numberStep = $i + 1;
+                                $this->stepDao->insertStep($numberStep, $listStep[$i], $recipeId);
+                            }
+                        }
                     }
-                } 
-                catch(Exception $ex) {
-                    // TODO : do something here when error
+                } catch (Exception $ex) {
+                    $this->setUpdateError($ex->getMessage());
                 }
             }
         }
@@ -123,26 +148,28 @@ class RecipeController extends Controller {
         return Response::view("addRecipe", ["categories" => $categories, "ingredients" => $ingredients, "units" => $units]);
     }
 
-    public function delete(Request $request) {
+    public function delete(Request $request)
+    {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         $post = $request->request();
         $id = intval($post->get('id'));
-        
+
         $this->stepDao->deleteStepByRecipeId($id);
-        $this->recipeIngredientDao->deleteIngredientFromRecetteId($id);
+        $this->recipeIngredientDao->deleteRecipeIngredientByRecipeId($id);
         $this->recipeDao->deleteRecipeById($id);
 
-        return Response::json(["success" => true]);          
+        return Response::json(["success" => true]);
     }
 
-    public function edit(Request $request) { 
+    public function edit(Request $request)
+    {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        
-        $arguments = $request->routeArguments();
-        $id = intval($arguments->get('id'));  
 
-        if(is_int($id)) {
+        $arguments = $request->routeArguments();
+        $id = intval($arguments->get('id'));
+
+        if (is_int($id)) {
             $recipe = $this->recipeDao->getRecipeById($id);
             $recipeIngredient = $this->recipeIngredientDao->getIngredientByIdRecipe($id);
             $recipeStep = $this->stepDao->getStepByRecipeId($id);
@@ -152,26 +179,26 @@ class RecipeController extends Controller {
             $units = $this->unitDao->getUnits();
 
 
-            return Response::view("editRecipe",['recipe' => $recipe, "recipeIngredients" => $recipeIngredient, "recipeSteps" => $recipeStep, "categories" => $categories, "ingredients" => $ingredients, "units" => $units]);
-        } 
+            return Response::view("editRecipe", ['recipe' => $recipe, "recipeIngredients" => $recipeIngredient, "recipeSteps" => $recipeStep, "categories" => $categories, "ingredients" => $ingredients, "units" => $units]);
+        }
 
         return Response::view("404");
     }
 
-    public function update(Request $request) {
+    public function update(Request $request)
+    {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         $categories = [];
-        
+
         $post = $request->request();
         $userId = $_SESSION['user']->getId();
 
         $arguments = $request->routeArguments();
-        $recipeId = intval($arguments->get('id')); 
-        
-        
-        if($post->count() > 0) {
-            // TODO : return redirection vers une page
+        $recipeId = intval($arguments->get('id'));
+
+
+        if ($post->count() > 0) {
             $name = $post->get('name');
             $cost = $post->get('cost');
             $prepTime = $post->get('prep_time');
@@ -182,47 +209,93 @@ class RecipeController extends Controller {
             $listQuantity = $post->get('quantity');
             $listUnit = $post->get('unit');
             $listStep = $post->get('step');
-        
-            if(strlen($name) > 0
-            && strlen($cost) > 0
-            && strlen($prepTime) > 0
-            && strlen($cookingTime) > 0
-            && strlen($headcount) > 0
-            && strlen($recipeCategory) > 0
-            && is_array($listIngredient)
-            && is_array($listQuantity)
-            && is_array($listUnit)
-            && is_array($listStep)) {
+
+            // verifier que recipe-image existe
+            $recipeImageName = $_FILES["recipe-image"]["name"];
+            $recipeImageSize = $_FILES["recipe-image"]["size"];
+
+            if (
+                strlen($name) > 0
+                && strlen($cost) > 0
+                && strlen($prepTime) > 0
+                && strlen($cookingTime) > 0
+                && strlen($headcount) > 0
+                && strlen($recipeCategory) > 0
+                && is_array($listIngredient)
+                && is_array($listQuantity)
+                && is_array($listUnit)
+                && is_array($listStep)
+            ) {
 
                 $releaseDate = date("Y-m-d");
                 try {
                     $mapRecipeCategory = $this->recipeCategoryDao->getRecipeCategoryByName($recipeCategory);
                     $IdRecipeCategory = $mapRecipeCategory->getIdCategory();
-                    $this->recipeDao->updateRecipeById($recipeId, $name, $cost, $prepTime, $cookingTime, $releaseDate, 1, $headcount, intval($IdRecipeCategory));
-                
-                    for($i = 0; $i < count($listIngredient); $i++) {
-                        $ingredientId = $this->ingredientDao->getIngredientIdByName($listIngredient[$i]);
-                        $unitId = $this->unitDao->getUnitByLabel($listUnit[$i]);
-                        $this->recipeIngredientDao->updateIngredientByRecipeIngredientId(intval($ingredientId), intval($listQuantity[$i]), intval($unitId), intval($recipeId));
+
+                    // On vérifie si on a remplie l'input file
+                    if (!empty($recipeImageName)) {
+                        $uploadedDate = date("d-m-Y_H-i-s");
+
+                        $ext = explode('.', $recipeImageName);
+                        $extensionFile = $ext[1];
+
+                        $newFileName = $ext[0] . '_' . $uploadedDate;
+                        $targetPath = $_SERVER['DOCUMENT_ROOT'] . 'marmitonCnam/public/recipe-images/' . $newFileName . '.' . $extensionFile;
+
+                        // Si la taille de l'image est au dessus de 4MB.
+                        if ($recipeImageSize < 4194304) {
+                            if (move_uploaded_file($_FILES['recipe-image']['tmp_name'], $targetPath)) {
+                                $this->recipeDao->updateRecipeById($recipeId, $name, $cost, $prepTime, $cookingTime, $releaseDate, 1, $headcount, $newFileName . "." . $extensionFile, intval($IdRecipeCategory));
+                                $this->updateIngredients($listIngredient, $listQuantity, $listUnit, $recipeId);
+                                $this->updateSteps($listStep, $listQuantity, $listUnit, $recipeId); 
+                            }
+                        }
+                    } else {
+                        $this->recipeDao->updateRecipeByIdWithoutImage($recipeId, $name, $cost, $prepTime, $cookingTime, $releaseDate, 1, $headcount, intval($IdRecipeCategory)); 
+                        $this->updateIngredients($listIngredient, $listQuantity, $listUnit, $recipeId);
+                        $this->updateSteps($listStep, $listQuantity, $listUnit, $recipeId);
                     }
 
-      
-                    for($i = 0; $i < count($listStep); $i++) {
-                        $numberStep = $i + 1;
-
-                        
-                        $this->stepDao->updateStepByRecipeId($recipeId, $numberStep, $listStep[$i]);
-                    }
                     $this->redirect($this->location);
-                } 
-                catch(Exception $ex) {
-                    $this->setUpdateError($ex->getMessage());      
-                    // TODO : do something here when error
+                } catch (Exception $ex) {
+                    $this->setUpdateError($ex->getMessage());
                 }
             } else {
                 $this->setUpdateError("Les champs ne peuvent pas être vides");
             }
             $this->redirect(Configuration::get('index') . '/admin/recipe/' . $recipeId . "/edit");
+        }
+    }
+
+    public function consult(Request $request)
+    {
+        $arguments = $request->routeArguments();
+        $id = intval($arguments->get('id'));
+
+        if (is_int($id)) {
+            $recipe = $this->recipeDao->getRecipeById($id);
+            $recipeIngredient = $this->recipeIngredientDao->getIngredientByIdRecipe($id);
+            $recipeStep = $this->stepDao->getStepByRecipeId($id);
+
+            return Response::view("consultRecipe", ["recipe" => $recipe, "recipeIngredients" => $recipeIngredient, "recipeSteps" => $recipeStep]);
+        }
+        return Response::view("404");
+    }
+
+    private function updateIngredients($listIngredient, $listQuantity, $listUnit, $recipeId) {
+        for ($i = 0; $i < count($listIngredient); $i++) {
+            $ingredientId = $this->ingredientDao->getIngredientIdByName($listIngredient[$i]);
+            $unitId = $this->unitDao->getUnitByLabel($listUnit[$i]);
+            $this->recipeIngredientDao->insertIfNotExistIngredientByRecipeId(intval($recipeId), intval($ingredientId), intval($listQuantity[$i]), intval($unitId));
+            $this->recipeIngredientDao->updateIngredientByRecipeIngredientId(intval($ingredientId), intval($listQuantity[$i]), intval($unitId), intval($recipeId));
+        }
+    }
+
+    private function updateSteps($listStep, $listQuantity, $listUnit, $recipeId) {
+        for ($i = 0; $i < count($listStep); $i++) {
+            $numberStep = $i + 1;
+            $this->stepDao->insertIfNotExistStepByRecipeId($numberStep, $listStep[$i], $recipeId);
+            $this->stepDao->updateStepByRecipeId($recipeId, $numberStep, $listStep[$i]);
         }
     }
 
